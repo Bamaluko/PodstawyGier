382a00bef10869270ba6f187434047c56771e13d
Bamaluko <kuchciakkacper@gmail.com>
2022-11-05 18:22:45 +0100

    Revert "Merge remote-tracking branch 'origin/abillities'"
    This reverts commit cadb6d1834099cc730b63309786ceb8521663924, reversing
    changes made to 3138bcb9dea57becd395624cfc2bbe7721536760.

--- Assets/Scripts/AbillityUnlock.cs
+++ Assets/Scripts/AbillityUnlock.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class AbillityUnlock : MonoBehaviour
 {
     public bool unlockDoubleJump, unlockDash, unlockBecomeBall, unlockDropBomb;
-    public GameObject pickupEffect;
-
-    public string unlockMessage;
-    public TMP_Text unlockText;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -37,31 +32,6 @@ public class AbillityUnlock : MonoBehaviour
                 player.canDropBomb = true;
             }
 
-            if (player.canDoubleJump && player.canDash && player.canBecomeBall && player.canDropBomb) unlockMessage = "All abillities unlocked!";
-            else if (player.canDoubleJump && player.canDash && player.canBecomeBall) unlockMessage = "Double jump, Dash and change to ball unlocked!";
-            else if (player.canDoubleJump && player.canDash && player.canDropBomb) unlockMessage = "Double jump, Dash and Drop bomb unlocked!";
-            else if (player.canDoubleJump && player.canDropBomb && player.canBecomeBall) unlockMessage = "Double jump, Change to ball and Drop bomb unlocked!";
-            else if (player.canDash && player.canDropBomb && player.canBecomeBall) unlockMessage = "Dash, Change to ball and Drop bomb unlocked!";
-            else if (player.canDoubleJump && player.canDash) unlockMessage = "Double jump and Dash unlocked!";
-            else if (player.canDoubleJump && player.canBecomeBall) unlockMessage = "Double jump and Change to ball unlocked!";
-            else if (player.canDoubleJump && player.canDropBomb) unlockMessage = "Double jump and Drop bomb unlocked!";
-            else if (player.canDash && player.canBecomeBall) unlockMessage = "Dash and Change to ball unlocked!";
-            else if (player.canDash && player.canDropBomb) unlockMessage = "Dash and Drop bomb unlocked!";
-            else if (player.canBecomeBall && player.canDropBomb) unlockMessage = "Change to ball and Drop bomb unlocked!";
-            else if (player.canDoubleJump) unlockMessage = "Double Jump unlocked!";
-            else if (player.canDash) unlockMessage = "Dash unlocked!";
-            else if (player.canBecomeBall) unlockMessage = "Change to ball unlocked!";
-            else if (player.canDropBomb) unlockMessage = "Drop bomb unlocked!";
-
-            Instantiate(pickupEffect, transform.position, transform.rotation);
-
-            unlockText.transform.parent.SetParent(null);
-            unlockText.transform.parent.position = transform.position;
-
-            unlockText.text = unlockMessage;
-            unlockText.gameObject.SetActive(true);
-
-            Destroy(unlockText.transform.parent.gameObject, 5f);
             Destroy(gameObject);
         }
     }
