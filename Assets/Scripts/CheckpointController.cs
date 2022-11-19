fac08c0c08c2dcbc0ba2bf8b4cd5c4e29e427ba8
Bamaluko <kuchciakkacper@gmail.com>
2022-11-19 12:16:30 +0100

    level_design: Room1 mostly finished

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public GameObject healEffect;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            RespawnController.instance.setSpawn(transform.position);
+            PlayerHealthController.instance.FillHealth();
+
+            PlayerPrefs.SetFloat("PosX", transform.position.x);
+            PlayerPrefs.SetFloat("PosY", transform.position.y);
+            PlayerPrefs.SetFloat("PosZ", transform.position.z);
+
+            Instantiate(healEffect, transform.position, transform.rotation);
+        }
+    }
+}
