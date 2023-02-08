3f4d20d4a1ed604910865b9ef5ab1592d7fcd9b5
bzzak <bartek.zakk@gmail.com>
2023-02-08 01:02:24 +0100

    feat: add tutorial text to firsts checkpoints

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckpointController : MonoBehaviour
 {
     public GameObject healEffect;
+    public TMP_Text info;
+    public float infoShowTime;
+    private bool showInfo = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player")
         {
+            if(info != null)
+            {
+                info.gameObject.SetActive(true);
+                showInfo = true;
+            }
             RespawnController.instance.setSpawn(transform.position);
             PlayerHealthController.instance.FillHealth();
 
@@ -19,6 +28,16 @@ public class CheckpointController : MonoBehaviour
 
             Instantiate(healEffect, transform.position, transform.rotation);            
             AudioManager.instance.PlaySFX(5);
+        }
+    }
+
+    public void Update()
+    {
+        if (showInfo && infoShowTime >= 0) infoShowTime -= Time.deltaTime;
+        else if(showInfo)
+        {
+            showInfo = false;
+            info.gameObject.SetActive(false);
         }
     }
 }
